0ea46d2ee9043875f82757607182c95f37399eee
WarFade <96909243+WarFade@users.noreply.github.com>
2024-09-30 04:40:31 +0530

    Create copyText.js

---
+++ api/copyText.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+app.get('/copy', (req, res) => {
+    const text = req.query.text ? decodeURIComponent(req.query.text) : '';
+    res.json({ copiedText: text });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
